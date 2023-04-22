f5296ba557cf0aef7a9aa51bca0ae289426fc489
csxmli2016 <csxmli@gmail.com>
2023-04-22 22:36:33 +0800

    Add files via upload

---
+++ checkpoints/download_github.py
@@ -0,0 +1,5 @@
+from basicsr.utils.download_util import load_file_from_url
+
+load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_prior_generation.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_sr.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_transformer_encoder.pth', model_dir='./checkpoints')
