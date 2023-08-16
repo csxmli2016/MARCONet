5888d7636fbf5a7d3c3d8b081211b66e8930d2c7
csxmli2016 <csxmli@gmail.com>
2023-08-16 20:14:35 +0800

    Update download_github.py

--- checkpoints/download_github.py
+++ checkpoints/download_github.py
@@ -1,5 +1,12 @@
 from basicsr.utils.download_util import load_file_from_url
 
+# For Chinese
 load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_prior_generation.pth', model_dir='./checkpoints')
 load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_sr.pth', model_dir='./checkpoints')
 load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_transformer_encoder.pth', model_dir='./checkpoints')
+
+
+# For English and Number
+load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/Eng/net_prior_generation_Eng.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/Eng/net_sr_Eng.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/Eng/net_transformer_encoder_Eng.pth', model_dir='./checkpoints')
