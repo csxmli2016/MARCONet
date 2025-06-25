b0ee20f10eda0a400f49e7b065473c8ee04834c2
csxmli2016 <csxmli@gmail.com>
2025-06-25 21:07:07 +0800

    Update yolo11m for character detection

--- checkpoints/download_github.py
+++ checkpoints/download_github.py
@@ -4,5 +4,8 @@ from basicsr.utils.download_util import load_file_from_url
 load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_prior_generation.pth', model_dir='./checkpoints')
 load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_sr.pth', model_dir='./checkpoints')
 load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_transformer_encoder.pth', model_dir='./checkpoints')
-load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_new_bbox.pth', model_dir='./checkpoints')
-load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_real_world_ocr.pth', model_dir='./checkpoints')
+# load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_new_bbox.pth', model_dir='./checkpoints')
+# load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/net_real_world_ocr.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONet/releases/download/v1/yolo11m_character.pt', model_dir='./checkpoints')
+
+
